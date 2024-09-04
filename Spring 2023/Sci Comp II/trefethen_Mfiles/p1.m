65b18fdba48880a14143533e1fb9077c4795ecb9
StephAStark <dallask9944@gmail.com>
2024-09-04 12:05:59 -0600

    Finished adding back files

--- Spring 2023/Sci Comp II/trefethen_Mfiles/p1.m
+++ Spring 2023/Sci Comp II/trefethen_Mfiles/p1.m
@@ -1,12 +1,9 @@
-close all
-clc
 % p1.m - convergence of fourth-order finite differences
 
 % For various N, set up grid in [-pi,pi] and function u(x):
-  Nvec= 2.^(3:16);
+  Nvec = 2.^(3:12);
   clf, subplot('position',[.1 .4 .8 .5])
   for N = Nvec
-      tic
     h = 2*pi/N; x = -pi + (1:N)'*h;
     u = exp(sin(x)); uprime = cos(x).*u;
 
@@ -18,20 +15,9 @@ clc
 
     % Plot max(abs(D*u-uprime)):
     error = norm(D*u-uprime,inf);
-    loglog(N,error,'.','markersize',15),
-    hold on;
-    T=toc;
-    %scatter(N,T);
-    %hold on;
-
+    loglog(N,error,'.','markersize',15), hold on
   end
   grid on, xlabel N, ylabel error
   title('Convergence of fourth-order finite differences')
-  semilogy(Nvec,Nvec.^(-4),'--')
+  semilogy(Nvec,Nvec.^(-4),'--') 
   text(105,5e-8,'N^{-4}','fontsize',18)
-  %grid on, xlabel N, ylabel Time
-  %title('Computation time for Different N Values')
-
-% Uncomment out the last two lines in the for loop and the last two lines
-% above while commenting out the loglog line and lines 28-31 to get the
-% code used to generate the time graph.
